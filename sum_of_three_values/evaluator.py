a025a4b2b12bb8980231e350de77ea9b95b34fea
ubuntu <visotruce@gmail.com>
2019-07-09 20:21:49 +0200

    [FIX] fixed problems with sum_of_three_values, now it works fine.

--- sum_of_three_values/evaluator.py
+++ sum_of_three_values/evaluator.py
@@ -1,11 +1,8 @@
 import random
 
-import turingarena as ta
+from turingarena import *
 from turingarena.evallib.algorithm import run_algorithm
 
-num_tests = 20
-N = 10
-        
 def get_sum(array):
     done = False
     while(done==False):
@@ -33,39 +30,39 @@ def is_correct(array,indexes,sum):
         return False
 
 
-
+num_tests = 20
+N = 10
+        
+all_passed = True
 for _ in range(num_tests):
     # create the array in a random way
     array = []
     for _ in range(N):
-        array.append(random.randint(0,10 ** 5))
+        array.append(random.randint(0,10 ** 2))
 
     # take a random sum of 3 distinct elements of the array
     correct = get_sum(array)
 
-    try:
-        print(f"Testing {array} with sum {correct} ...", end=" ")
-        indexes = [1,2,3]
-        with ta.run_algorithm(ta.submission.source) as process:
-            answer = process.functions.find_sum(array,N,correct)
-            # indexes[0] = process.functions.get_result(0)
-            # indexes[1] = process.functions.get_result(1)
-            # indexes[2] = process.functions.get_result(2)
-        
-        if is_correct(array,indexes,correct)==True:
-            print('correct, number of guess')
-        else:
-            print('WRONG!')
-            ta.goals["correct"] = False
-    #     print(f" answer: {c} {d}", end="\t")
-    #     if c == a + b:
-    #         print(" (correct)", end=" ")
-    #     else:
-    #         print("  (WRONG!)", end=" ")
-    #         ta.goals["correct"] = False
-    #     print(f"(time: {int(process.time_usage * 1000000)} us)")
-    except ta.AlgorithmError as e:
-        print(f" error: {e}")
-        ta.goals["correct"] = False
+    print(f"Testing {array} with sum {correct} ...", end=" ")
 
-ta.goals.setdefault("correct", True)
+    try:
+        with run_algorithm(submission.source) as process:
+            process.procedures.find_sum(N, array, correct)
+            indexes = [process.functions.get_result(i) for i in range(0,3)]
+        
+            if is_correct(array,indexes,correct)==True:
+                print(f"(correct) \t indexes: {indexes}")
+            else:
+                print("WRONG!")
+                all_passed = False
+                goals["correct"] = False
+        print(f"(time: {int(process.time_usage * 1000000)} us)",end="\n\n")
+    except AlgorithmError as e:
+        print(f" error: {e}")
+        goals["correct"] = False
+if(all_passed==True):
+    print("All test passed correctly.")
+else:
+    print("NOT all test passed correctly...")
+
+goals.setdefault("correct", True)
--- sum_of_three_values/solutions/correct.cpp
+++ sum_of_three_values/solutions/correct.cpp
@@ -1,23 +1,27 @@
-int res[3];
+#include <vector>
+#include <algorithm>
 
-int find_sum(int* array, int n, int sum) {
+std::vector<int> res(3);
+
+void find_sum(int n, int* a, int sum) {
     int temp_sum = 0;
+
     for(int i=0; i<n; i++) {
         for(int j=1; j<n; j++) {
             for(int k=2; k<n; k++) {
-                temp_sum = array[i] + array[j] + array[k];
+                temp_sum = a[i] + a[j] + a[k];
                 if(sum==temp_sum) {
                     res[0] = i;
                     res[1] = j;
                     res[2] = k;
-                    return 1;
+                    return;
                 }
             }
         }
     }
-    return 0;
 }
 
 int get_result(int i) {
     return res[i];
 }
+
